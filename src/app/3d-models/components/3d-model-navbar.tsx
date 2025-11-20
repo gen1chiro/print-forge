5d2d1c5138795c60b031a548b7fb96549e8c59dc
Jul Leo Javellana <juljavellana1218@gmail.com>
2025-11-20 17:22:26 +0800

    chore: active nav styling

--- src/app/3d-models/components/3d-model-navbar.tsx
+++ src/app/3d-models/components/3d-model-navbar.tsx
@@ -1,12 +1,23 @@
+'use client'
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { getAllCategories } from "@/lib/categories";
 
 
 export default function ModelNavbar() {
+    const pathname = usePathname();
     const categories = getAllCategories();
+    console.log(pathname);
+
     const categoryLinks = categories.map((category, index) => (
         <li key={index}>
-            <Link href={`/3d-models/categories/${category.slug}`}>{category.displayName}</Link>
+            <Link
+                href={`/3d-models/categories/${category.slug}`}
+                className={`hover:text-orange-300 ${pathname === `/3d-models/categories/${category.slug}` ? 'text-orange-300 underline' : 'text-black'}`}
+            >
+                {category.displayName}
+            </Link>
         </li>
     ));
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import printforgeLogo from "@/assets/images/printforge-logo.png";
 import printforgeLogoMobile from "@/assets/images/printforge-logo-mobile.png";
 
 const Navbar = () => {
+    const pathname = usePathname();
 
     return (
         <header className="fixed top-0 left-1/2 -translate-x-1/2 w-11/12 max-w-7xl mx-auto py-8 flex items-center justify-between bg-white z-10">
@@ -19,8 +23,22 @@ const Navbar = () => {
                 />
             </Link>
             <ul>
-                <li className="inline-block px-4 text-xl uppercase"><Link href="/3d-models">3D Models</Link></li>
-                <li className="inline-block px-4 text-xl uppercase"><Link href="/about">About</Link></li>
+                <li className="inline-block px-4 text-xl uppercase">
+                    <Link
+                        href="/3d-models"
+                        className={pathname === '/3d-models' ? 'text-orange-300 underline' : ''}
+                    >
+                        3D Models
+                    </Link>
+                </li>
+                <li className="inline-block px-4 text-xl uppercase">
+                    <Link
+                        href="/about"
+                        className={pathname === '/about' ? 'text-orange-300 underline' : ''}
+                    >
+                        About
+                    </Link>
+                </li>
             </ul>
         </header>
     )
